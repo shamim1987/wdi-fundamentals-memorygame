6ac5a8a6b2aed782d1fda3cbadb192655092fa70
Shamim Bhagat <Shamim Bhagat>
2016-09-17 19:01:43 +0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if ((cardOne === cardFour) || (cardTwo === cardThree)) {
+	alert ("You found a match.");
+} else {
+	alert ("Sorry, try again.");
+}
